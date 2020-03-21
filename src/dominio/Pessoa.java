b8f41e15592b453e42bcc020b00783801ca12d38
andresonsouza <andresonjazz@gmail.com>
2020-03-21 20:38:11 -0300

    Crie o construtor com argumentos

--- src/dominio/Pessoa.java
+++ src/dominio/Pessoa.java
@@ -1,11 +1,20 @@
 package dominio;
 
 public class Pessoa {
-	private interger id;
+	private Integer id;
 	private String name;
 	private String email;
 	
 	public Pessoa() {
 		
 	}
+
+	public Pessoa(Integer id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	
 }
