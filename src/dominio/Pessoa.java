fcd2e849f0291e1b11c8bde4ea978584e0aa11aa
andresonsouza <andresonjazz@gmail.com>
2020-03-21 21:32:21 -0300

    Implemente o serializable e adicione o id padrão

--- src/dominio/Pessoa.java
+++ src/dominio/Pessoa.java
@@ -1,6 +1,10 @@
 package dominio;
 
-public class Pessoa {
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private Integer id;
 	private String name;
 	private String email;
