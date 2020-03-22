995dff3a1c2cfe9ae453e60964ad739d41f25b73
andresonsouza <andresonjazz@gmail.com>
2020-03-21 21:46:54 -0300

    Instancie as pessoas

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,17 @@
 package aplicacao;
 
+import dominio.Pessoa;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Pessoa p1 = new Pessoa(1, "Andreson", "andresonjazz@gmail.com");
+		Pessoa p2 = new Pessoa(1, "Loide", "loideangelim@gmail.com");
+		Pessoa p3 = new Pessoa(1, "Vagdon", "vagdon@gmail.com");
+		System.out.print(p1);
+		System.out.print(p2);
+		System.out.print(p3);
 	}
 
 }
